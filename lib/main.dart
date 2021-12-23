20b8c2927dc952a5162c8aba7131cfc10ffc765d
Alireza Zamani <alirezazamani2922@gmail.com>
2021-12-23 17:59:34 +0330

    feat: Add flutter_graphql

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:matnbaz_mobile/screens/home.dart';
 import 'package:matnbaz_mobile/screens/owner.dart';
 import 'package:matnbaz_mobile/screens/repository.dart';
 
-void main() {
-  runApp(MaterialApp(
-    title: 'Matnbaz',
-    localizationsDelegates: const [
-      GlobalCupertinoLocalizations.delegate,
-      GlobalMaterialLocalizations.delegate,
-      GlobalWidgetsLocalizations.delegate,
-    ],
-    supportedLocales: const [
-      Locale("fa", "IR"),
-    ],
-    theme: ThemeData(
-      fontFamily: "Vazir",
-      primarySwatch: Colors.blue,
+void main() async {
+  // We're using HiveStore for persistence,
+  // so we need to initialize Hive.
+  await initHiveForFlutter();
+
+  final HttpLink httpLink = HttpLink(
+    'https://server.matnbaz.net/graphql',
+  );
+
+  final Link link = httpLink;
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: link,
+      // The default store is the InMemoryStore, which does NOT persist to disk
+      cache: GraphQLCache(store: HiveStore()),
     ),
-    locale: const Locale("fa", "IR"),
-    routes: {
-      '/': (context) => const HomeScreen(),
-      '/owner': (context) => const OwnerScreen(),
-      '/repository': (context) => const RepositoryScreen(),
-    },
-  ));
+  );
+
+  runApp(GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        title: 'Matnbaz',
+        localizationsDelegates: const [
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale("fa", "IR"),
+        ],
+        theme: ThemeData(
+          fontFamily: "Vazir",
+          primarySwatch: Colors.blue,
+        ),
+        locale: const Locale("fa", "IR"),
+        routes: {
+          '/': (context) => const HomeScreen(),
+          '/owner': (context) => const OwnerScreen(),
+          '/repository': (context) => const RepositoryScreen(),
+        },
+      )));
 }
